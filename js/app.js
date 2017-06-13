b16bda8914946e891c66c47eaf44cbf32b9e3f6b
Amanda <mrs.acapella@gmail.com>
2017-06-12 22:17:18 -0700

    mobile landing page finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 console.log("allo");
+$(function(){
+  // Begin Window Onload Function
+  console.log("but hello");
+
+
+  $( ".menu-container ").on('click', function() {
+    $( "#nav-elements" ).toggleClass("show");
+  });
+
+  // End Window Onload Function
+});
