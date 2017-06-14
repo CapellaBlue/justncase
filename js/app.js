7668046d6bed35e2a4d9e51415f77ef8aa0981e9
Amanda <mrs.acapella@gmail.com>
2017-06-13 21:56:02 -0700

    mobile site structure finished

--- js/app.js
+++ js/app.js
@@ -7,6 +7,26 @@ $(function(){
   $( ".menu-container ").on('click', function() {
     $( "#nav-elements" ).toggleClass("show");
   });
+  $("#about-link").on('click', function(){
+    // $('.about-j-modal').fadeIn("slow");
+    // $('.about-j-modal').css('visibility', 'visible').fadeIn("slow", "swing");
+    // $('.about-j-modal').fadeIn("slow", "swing");
+    $('.about-j-modal').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 2000);
+  });
+
+
+
+  $(window).scroll(function() {
+    console.log($(window).scrollTop());
+
+    if( $(window).scrollTop() > 2700){
+      console.log("scrolling");
+      $('.about-j-modal').css({opacity: 0, visibility: "visible"}).animate({opacity: 1}, 2000);
+    };
+
+
+ });
+
 
   // End Window Onload Function
 });
